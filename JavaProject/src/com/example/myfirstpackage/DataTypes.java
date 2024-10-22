b71293266a739645e252c87b02a00e219f661325
sunitha <sunitha.kanivalavan@vanavil-systems.com>
2024-10-22 14:02:52 -0400

    datatypes

--- JavaProject/src/com/example/myfirstpackage/DataTypes.java
+++ JavaProject/src/com/example/myfirstpackage/DataTypes.java
@@ -1,8 +1,46 @@
 package com.example.myfirstpackage;
 
+class car{
+	String brand;
+	int year;
+	car(String b, int y){
+		this.brand =b; // reference to the current object within the instance 
+		this.year = y;
+	}
+}
+
 public class DataTypes {
 
 	public static void main(String[] args) {
+		// primitive datatypes
+		byte age=25;
+		int salary = 20000;
+		float pi = 3.14f;
+		double percentage = 68.6;
+		char grade ='A';
+		boolean isDay =true;
+		
+		
+		System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Pi: " + pi);
+        System.out.println("percentage" +percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is now day " + isDay);
+        //non-primitive or reference datatype
+        
+        //string 
+        String name ="sunitha";
+        System.out.println("my name is " + name);
+        
+        //array
+        int[] numbers= {1,2,3,4,5};
+        System.out.println(numbers[4]);
+        //class and objects
+        car myCar = new car("Toyoto" ,2004);
+        System.out.println("car brand " +   myCar.brand);
+        System.out.println("year" + myCar.year);
+        
 
 	}
 
--- JavaProject/src/com/example/myfirstpackage/variables.java
+++ JavaProject/src/com/example/myfirstpackage/variables.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.myfirstpackage;
 import java.util.Scanner;
 
@@ -18,28 +18,4 @@ public class variables {
 		System.out.println(name);
 
 	}
-
 }
-=======
-package com.example.myfirstpackage;
-import java.util.Scanner;
-
-public class variables {
-
-	public static void main(String[] args) {
-	//	int a= 10;
-		//int b= 20;
-		//int c= a+b;
-		//System.out.println(c);
-		
-		Scanner obj = new Scanner(System.in);	//get user input from the console
-		int a= obj.nextInt();	//read next integer value from the user
-		int b= obj.nextInt();
-		String name=obj.nextLine();
-		//System.out.println(a+b);
-		System.out.println(name);
-
-	}
-
-}
->>>>>>> d3a39930482456ddb9ea3e7497f17a04776f485e

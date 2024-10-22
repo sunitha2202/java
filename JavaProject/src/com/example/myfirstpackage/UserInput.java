d3a39930482456ddb9ea3e7497f17a04776f485e
sunitha2202 <97227692+sunitha2202@users.noreply.github.com>
2024-10-22 01:45:38 -0400

    Add files via upload

---
+++ JavaProject/src/com/example/myfirstpackage/UserInput.java
@@ -0,0 +1,47 @@
+package com.example.myfirstpackage;
+
+import java.util.*;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// adding two variables getting input from user
+		// int a = scan.nextInt();
+		// int b = scan.nextInt();// Complete this line
+		// int c = scan.nextInt();// Complete this line
+
+		// System.out.println(a);
+		// System.out.println(b); // Complete this line
+
+		// System.out.println(c); // Complete this line
+
+		/*
+		 * String name =scan.nextLine(); int age = scan.nextInt(); scan.nextLine();
+		 * String address= scan.nextLine(); System.out.println("my name is " +name);
+		 * System.out.println("my age is" + age); System.out.println("my address is" +
+		 * address);
+		 */
+
+		/*
+		 * int a = scan.nextInt(); int b = scan.nextInt(); int c = scan.nextInt(); int d
+		 * = a * b * c; int e = a + b + c; int f = d / e; System.out.println(d);
+		 * System.out.println(e); System.out.println(f);
+		 */
+		String name =scan.nextLine();
+		double score = scan.nextDouble();
+		scan.nextLine();
+		String dept =scan.nextLine();
+		
+		System.out.println(name);
+		System.out.println(score/10);
+		System.out.println(dept);
+
+
+		
+
+		
+
+	}
+
+}
